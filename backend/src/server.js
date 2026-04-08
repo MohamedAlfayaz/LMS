4e7a483a627b7d54b2430aae0ed24cb51a99bf4e
MohamedAlfayaz <alfayaz23112004@gmail.com>
2026-04-08 16:36:03 +0530

    Url backend CORS change

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,26 +50,29 @@ if (!fs.existsSync(uploadsPath)) {
 /* ------------------ MIDDLEWARE ------------------ */
 
 // CORS
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://lms-tau-seven.vercel.app"
-];
+const allowedOrigins = process.env.CLIENT_URLS
+  ? process.env.CLIENT_URLS.split(",")
+  : [];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
+      // Allow no-origin (Postman, curl)
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
       }
+
+      return callback(
+        new Error(`CORS blocked: ${origin} not allowed`)
+      );
     },
     credentials: true,
   })
 );
+
+app.options("*", cors());
 
 // Body Parser
 app.use(express.json());
